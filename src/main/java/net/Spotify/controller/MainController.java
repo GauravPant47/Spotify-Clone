2a823336a6b1f84cc47c51891b97ec8c175f7ef4
Gaurav <gaurav556pant@gmail.com>
2023-05-23 17:42:58 +0530

    Compliting front end process

---
+++ src/main/java/net/Spotify/controller/MainController.java
@@ -0,0 +1,14 @@
+package net.Spotify.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/index")
+	public String mainPage() {
+		return "index";
+	}
+	
+}
