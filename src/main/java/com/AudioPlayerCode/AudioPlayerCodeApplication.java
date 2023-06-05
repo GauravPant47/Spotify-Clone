82642ec7d52d07bb54186693fc103289cd4ce5d0
Gaurav <gaurav556pant@gmail.com>
2023-06-05 19:06:07 +0530

    Working on the audio player

---
+++ src/main/java/com/AudioPlayerCode/AudioPlayerCodeApplication.java
@@ -0,0 +1,13 @@
+package com.AudioPlayerCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioPlayerCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioPlayerCodeApplication.class, args);
+	}
+
+}
